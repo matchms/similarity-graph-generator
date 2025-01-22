e0642e8782dc2d8055f347ea264fbc817298d444
sabroona <sabrina.cielas@gmail.com>
2025-01-22 01:53:35 +0100

    fix test

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -85,12 +85,10 @@ def some_scores(some_graph):
 def test_str(some_scores):
     expected_output = (
         "+++ SCORES FOR TEST +++\n"
-        "+++ Quality metrics +++\n"
         "Homogeneity: 0.85\n"
         "Completeness: 0.90\n"
         "Community Size: 0.75 with 1.00 communities and 1.00 avg community size\n"
-        "Modularity: 0.65\n"
-        "+++ Clustering metrics +++\n"
+        "Modularity: 0.65\n"+
         "Adjusted Rand Index: 0.78\n"
         "Normalized Mutual Information: 0.82\n"
         "Adjusted Mutual Information: 0.80\n"
